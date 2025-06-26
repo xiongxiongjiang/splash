a7be37cb607c73588b953b97eabf74e6e0c7d71a
徐荣 <243138640@qq.com>
2025-06-26 14:34:37 +0800

    feat:test nextConfig

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,6 +2,12 @@ import createNextIntlPlugin from 'next-intl/plugin';
 
 import type { NextConfig } from 'next';
 const withNextIntl = createNextIntlPlugin();
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+    experimental: {
+      serverComponentsExternalPackages: ['gsap']
+    },
+    // 启用静态优化
+    output: 'standalone'
+  };
 
 export default withNextIntl(nextConfig);
